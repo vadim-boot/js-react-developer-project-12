0a1081f9c6e3c4cab3981e659244fee6d6d03eeb
Vadim <gvadik@gmail.com>
2023-01-27 05:02:07 +0300

    Step 4.4.4

--- frontend/src/slices/channelSlice.js
+++ frontend/src/slices/channelSlice.js
@@ -8,11 +8,11 @@ const channelSlice = createSlice({
     initialState,
     reducers: {
         channelAdd: (state, action) => {
-            const {channel} = action.payload;
+            const channel = action.payload;
             state[channel.id] = channel;
         },
         channelRemove: (state, action) => {
-            const {id} = action.payload;
+            const id = action.payload;
             state = _.omit(state, [id]);
         }
     }
